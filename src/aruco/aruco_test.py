7c8ce7daf4f499fcd69d1185f3de7a8028d22e2b
char <codream00@gmail.com>
2024-12-30 11:10:15 +0900

    feat: aruco matrix

--- src/aruco/aruco_test.py
+++ src/aruco/aruco_test.py
@@ -1,5 +1,6 @@
 from .aruco import Aruco
 from .camera_util import CameraUtil
+from util.transform import create_transformation_matrix, transformation_3d, extract_rotation_translation
 import cv2
 import numpy as np
 
@@ -34,6 +35,16 @@ while True:
 
             # ID를 텍스트로 표시
             cv2.putText(frame, id_text, position, cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+
+            if ids[i][0] == 60:
+                r, _ = cv2.Rodrigues(rvecs[i])
+                t_1 = create_transformation_matrix(r, tvecs[i])
+                t_2 = transformation_3d(0,-np.pi/2,-np.pi/2,0,-0.4,-0.25)
+                t = np.dot(t_1, t_2)
+                base_r, base_tvec = extract_rotation_translation(t)
+                base_rvec, _ = cv2.Rodrigues(base_r)
+
+                cv2.drawFrameAxes(frame, mtx, dist, base_rvec, base_tvec, 0.2)
 
 
     
